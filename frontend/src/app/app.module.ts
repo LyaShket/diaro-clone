9b32367c0aac2e86eedb78553998422b5d9285e2
Dmytro Demjanenko <dmytro.demjanenko@gmail.com>
2022-10-29 20:13:17 +0300

    fill header, sidebar and footer

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,21 +1,31 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
 import {QuillConfigModule, QuillModule} from "ngx-quill";
 import {RouterModule, Routes} from "@angular/router";
-import { HomeComponent } from './pages/home/home.component';
-import { DiaryEntryComponent } from './pages/diary-entry/diary-entry.component';
+import {HomeComponent} from './pages/home/home.component';
+import {DiaryEntryComponent} from './pages/diary-entry/diary-entry.component';
 import {NgSelectModule} from "@ng-select/ng-select";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HeaderComponent} from './components/header/header.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    DiaryEntryComponent
+    DiaryEntryComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
@@ -24,10 +34,16 @@ import {NgSelectModule} from "@ng-select/ng-select";
     ReactiveFormsModule,
     HttpClientModule,
     QuillModule.forRoot(),
-    NgSelectModule
+    NgSelectModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent],
   exports: [],
 })
-export class AppModule { }
+export class AppModule {
+}
