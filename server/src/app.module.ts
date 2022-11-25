9cc8fee7c9c0da3adae8e0472f2301988228bd0a
Dmytro Demjanenko <dmytro.demjanenko@gmail.com>
2022-11-25 23:00:27 +0200

    add hello-world auth

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -5,13 +5,17 @@ import { DiaryEntryModule } from './modules/diary-entry/diary-entry.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DiaryTagModule } from './modules/diary-tag/diary-tag.module';
 import { DiaryCategoryModule } from './modules/diary-category/diary-category.module';
+import { AuthModule } from './modules/auth/auth.module';
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
     DiaryEntryModule,
     MongooseModule.forRoot('mongodb://127.0.0.1:27017'),
     DiaryTagModule,
-    DiaryCategoryModule
+    DiaryCategoryModule,
+    AuthModule,
+    UsersModule
   ],
   controllers: [AppController],
   providers: [AppService],
