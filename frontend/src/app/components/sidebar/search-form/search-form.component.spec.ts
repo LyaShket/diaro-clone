c6813e7bc0dfd58b5a24d435bbe06773b1cf57b8
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-01-03 12:49:07 +0200

    add tests for SearchFormComponent

--- frontend/src/app/components/sidebar/search-form/search-form.component.spec.ts
+++ frontend/src/app/components/sidebar/search-form/search-form.component.spec.ts
@@ -2,17 +2,23 @@ import { TestBed } from '@angular/core/testing';
 
 import { SearchFormComponent } from './search-form.component';
 import { Store } from '@ngxs/store';
+import { of } from 'rxjs';
+import Spy = jasmine.Spy;
+import SpyObj = jasmine.SpyObj;
 
-class MockService {}
 
 describe('SearchFormComponent', () => {
   let component: SearchFormComponent;
+  let dispatchSpy: Spy;
 
-  beforeEach(async () => {
+  beforeEach(() => {
+    const store = jasmine.createSpyObj<Store>('Store', ['dispatch']);
+    dispatchSpy = store.dispatch.and.returnValue(of({}));
+
     TestBed.configureTestingModule({
       providers: [
         SearchFormComponent,
-        { provide: Store, useClass: MockService },
+        { provide: Store, useValue: store },
       ],
     });
 
@@ -22,4 +28,113 @@ describe('SearchFormComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should build form', () => {
+    component.formValues = {
+      timeFrom: '2021-01-01',
+      timeTo: '2021-01-01',
+      text: 'text',
+      categories: ['category'],
+      tags: ['tag'],
+      moods: ['mood'],
+    };
+    component.ngOnChanges();
+    expect(component.form.value).toEqual(component.formValues);
+  });
+
+  it('should submit form and navigate search', () => {
+    component.submit();
+    expect(dispatchSpy.calls.count())
+      .withContext('spy method was called twice')
+      .toBe(2);
+  });
+
+  it('should add multiple categories to form', () => {
+    component.formValues = {
+      timeFrom: '2021-01-01',
+      timeTo: '2021-01-01',
+      text: 'text',
+      categories: [],
+      tags: ['tag'],
+      moods: ['mood'],
+    };
+    component.ngOnChanges();
+    component.clickCategory('category');
+    component.clickCategory('category2');
+    expect(component.form.value.categories).toEqual(['category', 'category2']);
+  });
+
+  it('should remove category from form', () => {
+    component.formValues = {
+      timeFrom: '2021-01-01',
+      timeTo: '2021-01-01',
+      text: 'text',
+      categories: ['category'],
+      tags: ['tag'],
+      moods: ['mood'],
+    };
+    component.ngOnChanges();
+    component.clickCategory('category');
+    expect(component.form.value.categories).toEqual([]);
+  });
+
+  it('should add multiple tags to form', () => {
+    component.formValues = {
+      timeFrom: '2021-01-01',
+      timeTo: '2021-01-01',
+      text: 'text',
+      categories: ['category'],
+      tags: [],
+      moods: ['mood'],
+    };
+    component.ngOnChanges();
+    component.clickTag('tag');
+    component.clickTag('tag2');
+    expect(component.form.value.tags).toEqual(['tag', 'tag2']);
+  });
+
+  it('should remove tag from form', () => {
+    component.formValues = {
+      timeFrom: '2021-01-01',
+      timeTo: '2021-01-01',
+      text: 'text',
+      categories: ['category'],
+      tags: ['tag'],
+      moods: ['mood'],
+    };
+    component.ngOnChanges();
+    component.clickTag('tag');
+    expect(component.form.value.tags).toEqual([]);
+  });
+
+  it('should add multiple moods to form', () => {
+    component.formValues = {
+      timeFrom: '2021-01-01',
+      timeTo: '2021-01-01',
+      text: 'text',
+      categories: ['category'],
+      tags: ['tag'],
+      moods: [],
+    };
+    component.ngOnChanges();
+    component.clickMood('mood');
+    component.clickMood('mood2');
+    expect(component.form.value.moods).toEqual(['mood', 'mood2']);
+  });
+
+  it('should remove mood from form', () => {
+    component.formValues = {
+      timeFrom: '2021-01-01',
+      timeTo: '2021-01-01',
+      text: 'text',
+      categories: ['category'],
+      tags: ['tag'],
+      moods: ['mood'],
+    };
+    component.ngOnChanges();
+    component.clickMood('mood');
+    expect(component.form.value.moods).toEqual([]);
+  });
+
+
 });
