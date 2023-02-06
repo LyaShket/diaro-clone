ac0f682eb947d24d89b7ef94a5cd9ff395a13884
Dmytro Demyanenko <dmytro.demjanenko@gmail.com>
2023-02-06 17:53:50 +0200

    write e2e tests for login

---
+++ frontend/cypress/e2e/auth/login.cy.ts
@@ -0,0 +1,39 @@
+/// <reference types="cypress" />
+
+import { clientUrl } from '../../../src/app/shared/constants/url/url';
+
+context('Login', () => {
+  beforeEach(() => {
+    cy.visit(clientUrl);
+  })
+
+  it('display login button in header', () => {
+    cy.get('#header #log-in')
+      .should('have.text', 'Log In')
+      .should('exist');
+  })
+
+  it('open login modal', () => {
+    cy.get('#header #log-in').click();
+    cy.get('.modal').should('exist');
+    cy.get('.modal h2').should('have.text', 'Log In');
+  });
+
+  it('login with valid credentials', () => {
+    cy.get('#header #log-in').click();
+    cy.get('.modal #username').type('testuserss');
+    cy.get('.modal #password').type('testuserss');
+    cy.get('.modal #submit').click();
+    cy.get('.modal').should('not.exist');
+    cy.get('#toast-container .toast-success').should('contain.text', 'Login success');
+  });
+
+  it('do not login with invalid credentials', () => {
+    cy.get('#header #log-in').click();
+    cy.get('.modal #username').type('testuserss');
+    cy.get('.modal #password').type('testuserss1');
+    cy.get('.modal #submit').click();
+    cy.get('.modal').should('not.exist');
+    cy.get('#toast-container .toast-error').should('contain.text', 'Login error');
+  });
+})
---
+++ frontend/src/app/shared/constants/url/url.ts
@@ -0,0 +1,2 @@
+export const clientUrl = 'http://localhost:4200';
+export const serverUrl = 'http://localhost:3000';
